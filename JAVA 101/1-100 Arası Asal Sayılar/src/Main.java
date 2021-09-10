c9e16ac6cfc49a40d48d3b9af1d6021098d6eb7b
mrtkrkrt <muratkrkrt0711@gmail.com>
2021-09-10 18:10:48 +0300

    .

---
+++ JAVA 101/1-100 Arası Asal Sayılar/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 2; i < 100; i++) {
+            int count = 0;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) count++;
+            }
+            if (count == 0) System.out.print(i + " ");
+        }
+    }
+}
