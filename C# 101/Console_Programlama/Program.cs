75f6c827df8d845f0835eee9b21af08824a92dcd
Murat Karakurt <55550212+mrtkrkrt@users.noreply.github.com>
2022-01-15 19:02:51 +0300

    c#

---
+++ C# 101/Console_Programlama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("İsminiz : ");
+            string name = Console.ReadLine();
+            Console.Write("Soyisminiz : ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + surname + ". Hoşgeldiniz!");
+        }
+    }
+}
